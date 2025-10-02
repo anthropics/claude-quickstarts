2aa55bd18362387d9e246c75d8ba309981ab655c
Alex Paris <parisac@anthropic.com>
2025-10-02 15:53:59 -0700

    remove unused import

--- computer-use-demo/computer_use_demo/tools/groups.py
+++ computer-use-demo/computer_use_demo/tools/groups.py
@@ -4,7 +4,7 @@ from typing import Literal
 from .base import BaseAnthropicTool
 from .bash import BashTool20241022, BashTool20250124
 from .computer import ComputerTool20241022, ComputerTool20250124
-from .edit import EditTool20241022, EditTool20250124, EditTool20250429, EditTool20250728
+from .edit import EditTool20241022, EditTool20250429, EditTool20250728
 
 ToolVersion = Literal[
     "computer_use_20250124", "computer_use_20241022", "computer_use_20250429"
